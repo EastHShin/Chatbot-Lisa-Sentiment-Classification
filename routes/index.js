c4db252e287e22ad79d974ebd4a7e63eb1507ab8
EastHShin <64204666+EastHShin@users.noreply.github.com>
2021-08-04 00:26:32 +0900

    fix ejs

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,10 @@ const request = require('request')
 var router = express.Router();
 const axios = require('axios');
 
-let label_list = [];
-let answer_list = [];
-let user_chat_list = [];
-let time_list = [];
-let count = 0;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  
-  
-  res.render('index', { title: 'Chat with Lisa' ,
-      label: label_list,
-      ans: answer_list,
-      user: user_chat_list,
-      time: time_list,
-      cnt: count
+  res.render('index', { title: 'Chat with Lisa'
   });
 });
 
@@ -26,13 +14,6 @@ router.post('/chat', (req,res,next) => {
   let result;
   let user_chat;
 
-  if(answer_list.length > 10){
-    label_list.length = 0;
-    answer_list.length = 0;
-    user_chat_list.length = 0;
-    time_list.length = 0;
-    count = 0;
-}
   const dateObj = new Date();
   let hours = dateObj.getHours();
   let minutes = dateObj.getMinutes();
@@ -40,12 +21,10 @@ router.post('/chat', (req,res,next) => {
     minutes = "0" + minutes
   }
   let local_time = hours + ':' + minutes;
-  time_list.push(local_time);
-  time_list.push(local_time);
 
   user_chat = req.body["user"];
   console.log(user_chat);
-  user_chat_list.push(user_chat);
+  
   const options = {
     uri: "https://main-chatbot-sentiment-classification-api-east-h-shin.endpoint.ainize.ai/chat",
     method: 'POST',
@@ -61,8 +40,7 @@ router.post('/chat', (req,res,next) => {
     result = await JSON.parse(body)
     label = await result['0']['label']
     answer = await result['1']
-    count += 2;
-    console.log(count + "server");
+    
     if (label == 'LABEL_0')
       trans_label = 'sadness';
     else if(label == 'LABEL_1')
@@ -76,18 +54,13 @@ router.post('/chat', (req,res,next) => {
     else if(label == 'LABEL_5')
       trans_label = 'Surprise';
 
-
-    label_list.push(trans_label);
-    answer_list.push(answer);
     console.log(trans_label, user_chat, answer);
-    console.log(label_list, answer_list, user_chat_list);
 
-    res.send({ title: 'Chatbot-Sentiment-Classification' ,
-    label: label_list,
-    ans: answer_list,
-    user: user_chat_list,
-    time: time_list,
-    cnt: count
+    res.send({
+    label: trans_label,
+    ans: answer,
+    user: user_chat,
+    time: local_time
   });
   });
 
